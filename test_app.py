1741a49bca8dc783e98af5ae60220ee7de79ad6c
Vijay Balasubramaniam <vbalasu@gmail.com>
2023-03-31 06:22:43 -0700

    Added curl POST to test

--- test_app.py
+++ test_app.py
@@ -1,4 +1,4 @@
-# HTTP GET
+# CURL GET
 # curl "$(chalice url)produce/vijay_balasubramaniam_oetrta_kafka_test/hello_from_lambda"
 
 # HTTP POST string
@@ -6,3 +6,7 @@
 
 # HTTP POST json object
 # echo '{"topic": "vijay_balasubramaniam_oetrta_kafka_test", "message": {"hello": "world"}}' | http post "$(chalice url)produce"
+
+# OR USE CURL to POST a json object
+echo '{"topic": "vijay_balasubramaniam_oetrta_kafka_test", "message": {"hello": "world"}}' | curl -H "Content-Type: application/json" -X POST --data-binary @- $(chalice url)produce
+
